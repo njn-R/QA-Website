093b99847fad65397fb4d26489e0822c658fb95d
njn-R <neerajnaveen364@gmail.com>
2023-06-23 10:35:10 +0530

    Deleted node modules and added to gitignore

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -3,16 +3,20 @@ import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
     email: {
       type: String,
       required: true,
       unique: true,
     },
+    name: {
+      type: String,
+      required: true,
+    },
     password: {
+      type: String,
+      required: true,
+    },
+    role: {
       type: String,
       required: true,
     },
